fb3795a1feedc37a376b4f340f58ed9fa88f70a5
Stromnatic <retep62@freemail.hu>
2024-06-29 22:40:26 +0200

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace UploaderWithResume
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.localFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.remoteFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.browseLocalFileButton = new System.Windows.Forms.Button();
+            this.browseRemoteFolderButton = new System.Windows.Forms.Button();
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(708, 77);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 0;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 145);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(256, 171);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(81, 13);
+            this.speedLabel.TabIndex = 2;
+            this.speedLabel.Text = "Upload Speed: ";
+            // 
+            // localFilePathTextBox
+            // 
+            this.localFilePathTextBox.Location = new System.Drawing.Point(12, 41);
+            this.localFilePathTextBox.Name = "localFilePathTextBox";
+            this.localFilePathTextBox.Size = new System.Drawing.Size(369, 20);
+            this.localFilePathTextBox.TabIndex = 3;
+            // 
+            // remoteFilePathTextBox
+            // 
+            this.remoteFilePathTextBox.Location = new System.Drawing.Point(414, 41);
+            this.remoteFilePathTextBox.Name = "remoteFilePathTextBox";
+            this.remoteFilePathTextBox.Size = new System.Drawing.Size(369, 20);
+            this.remoteFilePathTextBox.TabIndex = 4;
+            // 
+            // browseLocalFileButton
+            // 
+            this.browseLocalFileButton.Location = new System.Drawing.Point(12, 12);
+            this.browseLocalFileButton.Name = "browseLocalFileButton";
+            this.browseLocalFileButton.Size = new System.Drawing.Size(149, 23);
+            this.browseLocalFileButton.TabIndex = 5;
+            this.browseLocalFileButton.Text = "Browse Local File";
+            this.browseLocalFileButton.UseVisualStyleBackColor = true;
+            this.browseLocalFileButton.Click += new System.EventHandler(this.browseLocalFileButton_Click);
+            // 
+            // browseRemoteFolderButton
+            // 
+            this.browseRemoteFolderButton.Location = new System.Drawing.Point(414, 12);
+            this.browseRemoteFolderButton.Name = "browseRemoteFolderButton";
+            this.browseRemoteFolderButton.Size = new System.Drawing.Size(149, 23);
+            this.browseRemoteFolderButton.TabIndex = 6;
+            this.browseRemoteFolderButton.Text = "Browse Remote Folder";
+            this.browseRemoteFolderButton.UseVisualStyleBackColor = true;
+            this.browseRemoteFolderButton.Click += new System.EventHandler(this.browseRemoteFolderButton_Click);
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.Location = new System.Drawing.Point(12, 207);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.ReadOnly = true;
+            this.rtbConsole.Size = new System.Drawing.Size(776, 61);
+            this.rtbConsole.TabIndex = 7;
+            this.rtbConsole.Text = "";
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(438, 171);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(48, 13);
+            this.progressLabel.TabIndex = 8;
+            this.progressLabel.Text = "Progress";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 280);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.rtbConsole);
+            this.Controls.Add(this.browseRemoteFolderButton);
+            this.Controls.Add(this.browseLocalFileButton);
+            this.Controls.Add(this.remoteFilePathTextBox);
+            this.Controls.Add(this.localFilePathTextBox);
+            this.Controls.Add(this.speedLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.uploadButton);
+            this.Name = "frmMain";
+            this.Text = "File Uploader With Resume";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.TextBox localFilePathTextBox;
+        private System.Windows.Forms.TextBox remoteFilePathTextBox;
+        private System.Windows.Forms.Button browseLocalFileButton;
+        private System.Windows.Forms.Button browseRemoteFolderButton;
+        private System.Windows.Forms.RichTextBox rtbConsole;
+        private System.Windows.Forms.Label progressLabel;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploaderWithResume
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void browseLocalFileButton_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog()) 
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    localFilePathTextBox.Text = openFileDialog.FileName;
+        }
+
+        private void browseRemoteFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    remoteFilePathTextBox.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void uploadButton_Click(object sender, EventArgs e)
+        {
+            string localFilePath = localFilePathTextBox.Text;
+            string remoteFolderPath = remoteFilePathTextBox.Text;
+            string remoteFilePath = Path.Combine(remoteFolderPath, Path.GetFileName(localFilePath));
+
+            await UploadFileWithResume(localFilePath, remoteFilePath);
+        }
+
+        private async Task UploadFileWithResume(string localFilePath, string remoteFilePath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(localFilePath);
+                long fileLength = fileInfo.Length;
+                long totalBytesUploaded = 0;
+
+                if (File.Exists(remoteFilePath))
+                {
+                    totalBytesUploaded = new FileInfo(remoteFilePath).Length;
+                }
+
+                FileStream localStream = null;
+                try
+                {
+                    localStream = new FileStream(localFilePath, FileMode.Open, FileAccess.Read);
+                    localStream.Seek(totalBytesUploaded, SeekOrigin.Begin);
+
+                    byte[] buffer = new byte[1024 * 1024]; // 1MB chunk size
+                    int bytesRead;
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    while (totalBytesUploaded < fileLength)
+                    {
+                        try
+                        {
+                            bytesRead = await localStream.ReadAsync(buffer, 0, buffer.Length);
+                            if (bytesRead == 0)
+                                break;
+
+                            using (FileStream remoteStream = new FileStream(remoteFilePath, FileMode.Append, FileAccess.Write, FileShare.None))
+                            {
+                                await remoteStream.WriteAsync(buffer, 0, bytesRead);
+                            }
+
+                            totalBytesUploaded += bytesRead;
+
+                            // Calculate speed
+                            double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+                            double speed = totalBytesUploaded / elapsedSeconds / 1024; // Speed in KB/s
+
+                            // Calculate kilobytes
+                            double totalKilobytesUploaded = totalBytesUploaded / 1024.0;
+                            double totalKilobytes = fileLength / 1024.0;
+
+                            // Update progress bar and speed label
+                            progressBar1.Invoke((Action)(() =>
+                            {
+                                progressBar1.Value = (int)(totalBytesUploaded / (double)fileLength * 100);
+                            }));
+
+                            speedLabel.Invoke((Action)(() =>
+                            {
+                                speedLabel.Text = $"{speed:F2} KB/s";
+                            }));
+
+                            progressLabel.Invoke((Action)(() =>
+                            {
+                                progressLabel.Text = $"{totalKilobytesUploaded:F2} KB / {totalKilobytes:F2} KB uploaded";
+                            }));
+                        }
+                        catch (IOException)
+                        {
+                            // Handle disconnection
+                            rtbConsole.Text = DateTime.Now + " Connection lost. Retrying... (IO Exception)";
+                            await Task.Delay(5000); // Wait for 5 seconds before retrying
+
+                            // Reopen the local file stream and seek to the current position
+                            localStream?.Dispose();
+                            localStream = new FileStream(localFilePath, FileMode.Open, FileAccess.Read);
+                            localStream.Seek(totalBytesUploaded, SeekOrigin.Begin);
+                        }
+                    }
+                }
+                finally
+                {
+                    localStream?.Dispose();
+                }
+
+                rtbConsole.Text = DateTime.Now + " Upload completed successfully.";
+            }
+            catch (Exception ex)
+            {
+                rtbConsole.Text = DateTime.Now + " Error: " + ex.Message;
+            }
+        }
+    }
+}
